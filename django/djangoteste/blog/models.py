935fb900bfcfb7952766a77d273be346d99600e0
ghoste <hypxwghoste@gmail.com>
2020-12-31 19:59:49 -0300

    atualização

--- django/djangoteste/blog/models.py
+++ django/djangoteste/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True) # nome da url Ex: site/blog/alguma-coisa
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
--- django/djangoteste/djangoteste/settings.py
+++ django/djangoteste/djangoteste/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fi47bl(fm#5t(1oysl^h&jz2(uu4umc7tv5tfdak)e^@y2z086'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
