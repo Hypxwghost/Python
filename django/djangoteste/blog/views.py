7a8ae45ac4a31a49ef0748a576eb3cc9a838eca2
ghoste <hypxwghoste@gmail.com>
2021-01-01 23:32:24 -0300

    Atualização

--- django/djangoteste/blog/views.py
+++ django/djangoteste/blog/views.py
@@ -1,3 +1,18 @@
+from .models import Post
+
+from django.views.generic import DetailView, ListView
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Post 
+
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+def index(request): 
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
--- django/djangoteste/djangoteste/urls.py
+++ django/djangoteste/djangoteste/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from django.conf.urls.static import static
+from django.conf import settings
+from blog import views
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
+
