230ddfa92c8922761437597d11cdf65055b82b26
Ghoste <hypxwghoste@gmail.com>
2021-01-17 16:46:14 -0200

    atualização

--- contator_de_linhas.py
+++ contator_de_linhas.py
@@ -4,6 +4,8 @@ from time import sleep
 
 linhas_cont_py = file_cont_py = linhas_cont_md = file_cont_md = linhas_cont_txt = file_cont_txt = linhas_cont_c = file_cont_c = linhas_cont_html = file_cont_html = linhas_cont_css = file_cont_css = linhas_cont_js = file_cont_js = 0
 
+cont = 0
+
 caminho = os.path.dirname(os.path.realpath(__file__))
 print(f'Pegando arquivos em: {caminho}')
 sleep(2)
@@ -68,17 +70,9 @@ for root, dirs, files in os.walk(caminho):
                 
 # Solução temporaria,ate eu achar uma melhor ._.
 
-if linhas_cont_py != 0:
-    print(f'Você tem {linhas_cont_py} linhas de codigo em {file_cont_py} arquivos .py')
-if linhas_cont_md != 0:
-    print(f'Você tem {linhas_cont_md} linhas em {file_cont_md} arquivos .md')
-if linhas_cont_txt != 0:
-    print(f'Você tem {linhas_cont_txt} linhas em {file_cont_txt} arquivos .txt')
-if linhas_cont_c != 0:
-    print(f'Você tem {linhas_cont_c} linhas em {file_cont_c} arquivos .c ')
-if linhas_cont_html != 0:
-    print(f'Você tem {linhas_cont_html} linhas em {file_cont_html} arquivos .html')
-if linhas_cont_css != 0:
-    print(f'Você tem {linhas_cont_css} linhas em {file_cont_css} arquivos .css')
-if linhas_cont_js != 0:
-    print(f'Você tem {linhas_cont_js} linhas em {file_cont_js} arquivos .js')
+lista = [[linhas_cont_py, file_cont_py], [linhas_cont_txt, file_cont_txt], [linhas_cont_md, file_cont_md], [linhas_cont_js, file_cont_js], [linhas_cont_html, file_cont_html], [linhas_cont_css, file_cont_css], [linhas_cont_c, file_cont_c]]
+extension = ['.py', '.txt', '.md', '.js', '.html', '.css', '.c']
+for item in lista:
+    cont += 1
+    if item[1] > 0:
+        print(f'Você tem {item[0]} linhas em {item[1]} arquivos {extension[cont-1]}')
