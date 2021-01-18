c743e236da5540f3a0b6f5c6ec6aacfb42dd84da
Ghoste <hypxwghoste@gmail.com>
2021-01-18 00:19:31 -0200

    organizaçãov2

--- contator_de_linhas.py
+++ contator_de_linhas.py
@@ -14,15 +14,15 @@ sleep(2)
 
 for root, dirs, files in os.walk(caminho):
     for file in files:
-        if file.endswith('md'): # Makrdown
+        if file.endswith('md'):  # Makrdown
             with open(root+'/'+str(file), 'r', errors='ignore') as f:
                 file_cont_md += 1
                 script_code = f.readlines()
-            
+
             for line in script_code:
                 linhas_cont_md += 1
 
-        if file.endswith('.py'): # Python
+        if file.endswith('.py'):  # Python
             with open(root+'/'+str(file), 'r', errors='ignore') as f:
                 file_cont_py += 1
                 script_code = f.readlines()
@@ -30,7 +30,7 @@ for root, dirs, files in os.walk(caminho):
             for line in script_code:
                 linhas_cont_py += 1
 
-        if file.endswith('.txt'): # Text
+        if file.endswith('.txt'):  # Text
             with open(root+'/'+str(file), 'r', errors='ignore') as f:
                 file_cont_txt += 1
                 script_code = f.readlines()
@@ -38,7 +38,7 @@ for root, dirs, files in os.walk(caminho):
             for line in script_code:
                 linhas_cont_txt += 1
 
-        if file.endswith('.c'): # C
+        if file.endswith('.c'):  # C
             with open(root+'/'+str(file), 'r', errors='ignore') as f:
                 file_cont_c += 1
                 script_code = f.readlines()
@@ -46,7 +46,7 @@ for root, dirs, files in os.walk(caminho):
             for line in script_code:
                 linhas_cont_c += 1
 
-        if file.endswith('.html'): # html
+        if file.endswith('.html'):  # html
             with open(root+'/'+str(file), 'r', errors='ignore') as f:
                 file_cont_html += 1
                 script_code = f.readlines()
@@ -54,7 +54,7 @@ for root, dirs, files in os.walk(caminho):
             for line in script_code:
                 linhas_cont_html += 1
 
-        if file.endswith('.css'): # css
+        if file.endswith('.css'):  # css
             with open(root+'/'+str(file), 'r', errors='ignore') as f:
                 file_cont_css += 1
                 script_code = f.readlines()
@@ -62,23 +62,23 @@ for root, dirs, files in os.walk(caminho):
             for line in script_code:
                 linhas_cont_css += 1
 
-        if file.endswith('.js'): # js
+        if file.endswith('.js'):  # js
             with open(root+'/'+str(file), 'r', errors='ignore') as f:
                 file_cont_js += 1
                 script_code = f.readlines()
 
             for line in script_code:
                 linhas_cont_js += 1
-                
-lista = [ 
-            [linhas_cont_py, file_cont_py],
-            [linhas_cont_txt, file_cont_txt], 
-            [linhas_cont_md, file_cont_md], 
-            [linhas_cont_js, file_cont_js], 
-            [linhas_cont_html, file_cont_html], 
-            [linhas_cont_css, file_cont_css], 
-            [linhas_cont_c, file_cont_c] 
-        ]
+
+lista = [
+    [linhas_cont_py, file_cont_py],
+    [linhas_cont_txt, file_cont_txt],
+    [linhas_cont_md, file_cont_md],
+    [linhas_cont_js, file_cont_js],
+    [linhas_cont_html, file_cont_html],
+    [linhas_cont_css, file_cont_css],
+    [linhas_cont_c, file_cont_c]
+]
 
 for item in lista:
     cont += 1
