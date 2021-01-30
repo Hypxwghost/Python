8c51a7d64bfba62c7a5cd7636049120ccdef9ee7
ghoste <hypxwghoste@gmail.com>
2021-01-30 17:25:02 -0300

    Adição de pasta para desafios

---
+++ desafios-python/de002.py
@@ -0,0 +1,13 @@
+hora_list = []
+
+hora = input("Digite um horário, Ex: 1:00Pm ou 13:00Am").upper()
+
+hora_list.append(hora)
+
+def AP(hora):
+    print(hora)
+
+def PA(hora):
+    pass
+
+AP(hora)
---
+++ desafios-python/de003.py
@@ -0,0 +1,5 @@
+def contador():
+    num = int(input('Digite um número: '))
+    print(f'O numero {num} é formado por ', len(str(num)),'Caracteres')
+
+contador()
