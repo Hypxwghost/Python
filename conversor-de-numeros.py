5b91b04c3111cbd0c64ea547edae00c606ff7fa0
Ghoste <hypxwghoste@gmail.com>
2021-02-14 18:21:46 -0200

    a

--- conversor-de-numeros.py
+++ conversor-de-numeros.py
@@ -1,31 +1,17 @@
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-numr = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
-num2 = []
+d_romanos = {'i':1, 'v':5, 'x':10, 'l':50, 'c':100, 'd':500, 'm':1000}
 
-cont1 = cont5 = cont10 = cont50 = cont100 = cont500 = cont1000 =  0
+i_inteiro = int()
+s_romano = input('Número romano: ')
 
-num = input('Digite um número romano: ').upper()
+l_romano = [d_romanos[letra] for letra in s_romano]
 
-for num in num:
-    if num == 'I':
-        cont1 += 1
-
-    elif num == 'V':
-        cont5 += 1
-
-    elif num == 'X':
-        cont10 += 1
-
-    elif num == 'L':
-        cont50 += 1
-
-    elif num == 'C':
-        cont100 += 1
-
-    elif num == 'D':
-        cont500 += 1
-
-    elif num == 'M':
-        cont1000 += 1
-
-print(cont1, cont5, cont10, cont50, cont100, cont500, cont1000)
+for id, num in enumerate(l_romano):
+    try:
+        if num < l_romano[id+1]:
+            i_inteiro -= num
+        else:
+            i_inteiro += num
+    except:
+        i_inteiro += num
+    
+print(i_inteiro)
