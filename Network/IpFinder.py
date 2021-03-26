6c551677fc2acc884b1e0e1a11190af41ba33572
Hypxwghost <60857997+Hypxwghost@users.noreply.github.com>
2021-03-26 14:47:10 -0300

    useless update

--- Network/IpFinder.py
+++ Network/IpFinder.py
@@ -1,10 +1,6 @@
 import socket
 from requests import get
 
-hostname = socket.gethostname()
-local_ip = socket.gethostbyname(hostname)
-public_ip = get('https://api.ipify.org').text
-
-print(f'Hostname: {hostname}')
-print(f'Local IP: {local_ip}')
-print(f'Public IP: {public_ip}')
+print('Hostname: ', socket.gethostname())
+print('Local IP: ', socket.gethostbyname(socket.gethostname()))
+print('Public IP: ', get('https://api.ipify.org').text)
