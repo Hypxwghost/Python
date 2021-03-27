821513216a5258b988fbd52c58979d9083a563d7
Hypxwghost <60857997+Hypxwghost@users.noreply.github.com>
2021-03-27 15:37:04 -0300

    fix chat

---
+++ TcpChatRoom/Client.py
@@ -0,0 +1,78 @@
+import socket
+
+import threading
+
+from getpass import getpass
+# Choosing nickname
+nickname = input('Choose your nickname: ')
+if nickname == 'admin':
+    password = getpass('Enter password for admin: ')
+
+# Connecting to server
+client =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55555))
+
+stop_thread = False
+
+# listening to server and sending nickname
+def receive():
+    while True:
+        global stop_thread
+        if stop_thread:
+            break
+        try:
+            # Receive message from server
+            # if "NICK" send nickname
+            message = client.recv(1024).decode('ascii')
+
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+                next_message = client.recv(1024).decode('ascii')
+
+                if next_message == 'PASS':
+                    client.send(password.encode('ascii'))
+
+                    if client.recv(1024).decode('ascii') == 'REFUSE':
+                        print('Connection was refused!')
+                        stop_thread = True
+
+                elif next_message == 'BAN':
+                    print('Connection refused because of ban!')
+                    client.close()
+                    stop_thread = True
+
+            else:
+                print(message)
+                
+        except:
+            # close connection when error
+            print('An error occurred!')
+            client.close()
+            break
+
+# sending message to server
+def write():
+    while True:
+        if stop_thread:
+            break
+        else:
+            message = f'{nickname}: {input("> ")}'
+
+            if message[len(nickname)+2:].startswith('/'):
+                if nickname == 'admin':
+                    if message[len(nickname)+2:].startswith('/kick'):
+                        client.send(f'KICK {message[len(nickname)+2+6:]}'.encode('ascii'))
+                    
+                    elif message[len(nickname)+2:].startswith('/ban'):
+                        client.send(f'BAN {message[len(nickname)+2+5:]}'.encode('ascii'))
+                else:
+                    print('Commands can only be executed by the admin!')
+            else:
+                client.send(message.encode('ascii'))
+
+# Starting Threads For Listening And Writing
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
