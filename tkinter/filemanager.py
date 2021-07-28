6789d92dd48f8e8d969844c9ffd7a1093af77f61
Khalli <60857997+Hypxwghost@users.noreply.github.com>
2021-07-28 15:31:46 -0300

    Update filemanager.py

--- tkinter/filemanager.py
+++ tkinter/filemanager.py
@@ -1,25 +1,51 @@
 import os
 import platform
 import tkinter.ttk as ttk
+
 from tkinter import *
+from tkinter import filedialog
 
-tk = Tk()
-
+# Prints connected drives Ex: 'C:' 'D:' ...
 drives = [chr(x) + ':' for x in range(65, 90) if os.path.exists(chr(x) + ':')]
-
 print(drives)
 
-def getDir():
-    listdir = os.listdir(os.getcwd())
+root = Tk()
 
-    list_lbl = Label(tk, text='a')
-    list_lbl.pack()
+def listDir():
+    listdir = os.listdir(os.getcwd())
 
     for x in listdir:
         print(x)
 
-def cd():
-    os.chdir('C:' + '\\')
-    getDir()
+def cd(dir):   
+    try:
+        os.chdir(f'{dir}' + '\\')
+        listDir()
+    except:
+        print("file/dir not found")
 
-tk.mainloop()
+def openFile(file):
+    #if file.endswith('.txt'):
+    try:
+        txt_edit = 'code'
+
+        text_files = ['.txt', '.py', '.c', '.cs', '.md', '.rs']
+
+        if file.endswith(text_files):
+            with open(file, 'r') as file:
+                os.system(f'{txt_edit} {file}')
+
+    except FileNotFoundError:
+        print("File not found")
+
+while True:
+    user_input = input('> ')
+    if user_input[0:2] == 'cd':
+        dir = user_input[3:]
+        if len(dir) > 0:
+            cd(dir)
+
+    elif user_input[0:3] == 'cat':
+        openFile(user_input[4:])
+
+root.mainloop()
